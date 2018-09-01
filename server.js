80c25723c6644a5edd7ea67b9a00b89c4c830bd8
KirdMNKY <mr.mike.soto@gmail.com>
2018-08-31 21:54:55 -0500

    can not get the server.js file to require the friends.js

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static("app/public"));
-
-
+app.use(bodyparser.json())
 // Home Route
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/app/public/home.html"));
@@ -50,11 +49,43 @@ app.get("/api/friends", function(req, res){
     res.sendFile(path.join(__dirname, "/app/data/friends.js"));
 });
 
-
+var friends = require("data/friends.js");
 // Collect Info
-app.post("/api/friends", function(req, res){
-    var match = req.body
-});
+app.post("/api/friends", function(req,res){
+    var friend = req.body;
+    console.log(friend.scores);
+    // Compare user score with friends
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+    // Check all current friends
+    for(var i=0; i<friends.length; i++){
+      var scoresDiff = 0;
+      // Compare scores
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      // Add results
+      scoresArray.push(scoresDiff);
+    }
+
+    // Look for best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    //return bestMatch data
+    var bff = friends[bestMatch];
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
+    friends.push(req.body);
+  });
 
 
 
